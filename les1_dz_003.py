75555559a3930893b51e43afa964c6d56c211ca8
Makhauri <makhaurir@mail.ru>
2020-04-12 02:43:25 +0300

    5 tasks

---
+++ les1_dz_003.py
@@ -0,0 +1,8 @@
+#3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+user_num = input('Введите число: ')
+
+
+sum_var = int(user_num) + int(user_num + user_num) + int(user_num + user_num + user_num)
+
+print(sum_var)
